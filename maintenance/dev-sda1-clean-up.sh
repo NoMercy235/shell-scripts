a8e1cc62edea8be8b3e0c61d743fd4c7ed2dee13
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2017-12-22 15:44:36 +0100

    Updated the dev-sda1-clean-up script to show the date and time as well. It will be run daily to preserve space.

--- maintenance/dev-sda1-clean-up.sh
+++ maintenance/dev-sda1-clean-up.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+currDate=`date +"%Y-%m-%d %T"`
+echo "${currDate}: Cleanup for dev/sda1."
+
 # For Ubuntu < 16.04
 # dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p' | xargs sudo apt-get -y purge
 
 # For Ubuntu >= 16.04
 apt-get autoremove
+
+currDate=`date +"%Y-%m-%d %T"`
+echo "${currDate}: End of cleanup."
 
 exit 0
