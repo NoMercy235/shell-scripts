46f8df4092857d71f0bc30f40fcd279471bb6b25
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2017-12-28 00:43:12 +0100

    Enforced the accept all option for the dev-sda1-clean-up script.

--- maintenance/dev-sda1-clean-up.sh
+++ maintenance/dev-sda1-clean-up.sh
@@ -7,7 +7,7 @@ echo "${currDate}: Cleanup for dev/sda1."
 # dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p' | xargs sudo apt-get -y purge
 
 # For Ubuntu >= 16.04
-apt-get autoremove
+apt-get -y autoremove
 
 currDate=`date +"%Y-%m-%d %T"`
 echo "${currDate}: End of cleanup."
