7a7db6c5610637cee5aba9938ded34a65e680791
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2019-09-28 18:35:54 +0300

    feat: install and setup crontab
    refactor: automated zsh install and skipped activation of the shell

--- scripts/vps-setup/install-dependencies.sh
+++ scripts/vps-setup/install-dependencies.sh
@@ -16,6 +16,7 @@ function checkDependency {
 
 checkDependency "ssh-keygen" "apt-get install -y openssh-client"
 checkDependency "git" "apt-get install -y git"
+checkDependency "nano" "apt-get install -y nano"
 checkDependency "curl" "apt-get install -y curl"
 checkDependency "sudo" "apt-get install -y sudo"
 checkDependency "docker" "../get-docker.sh && usermod -aG docker ${me}"
@@ -25,5 +26,10 @@ checkDependency "zsh" "apt-get install -y zsh"
 ohMyZshDir="${HOME}/.zshrc"
 if [ ! -f "$ohMyZshDir" ]; then
   echo "Installing zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  RUNZSH=no yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
+
+# Setup crontab
+crontabFilePath="${PWD}/../../utils/crontab"
+checkDependency "crontab" "apt-get install -y cron"
+crontab "$crontabFilePath"
