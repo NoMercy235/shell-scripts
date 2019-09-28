ad84a7137980c407ccdc51ac4ca23d633f257d96
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2019-09-28 19:51:14 +0300

    feat: show public key after generation

--- scripts/vps-setup/generate-ssh-key.sh
+++ scripts/vps-setup/generate-ssh-key.sh
@@ -2,6 +2,10 @@ sshDir="${HOME}/.ssh"
 if [ ! -d "$sshDir" ]; then
   echo "Setting up SSH"
   ssh-keygen -f "${sshDir}/id_rsa" -N ""
+  echo
+  echo "Your public key is"
+  cat "${sshDir}/id_rsa.pub"
+  echo
 else
   echo "Ssh directory found. Skipping ssh generation."
 fi
